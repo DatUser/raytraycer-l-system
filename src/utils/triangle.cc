4b9d181c3af11d9bc4e4b1210c6c764e40c71a67
amaury <amaurydu35@gmail.com>
2021-04-11 22:11:48 +0200

    [UPDATE] triangle] ;

--- src/utils/triangle.cc
+++ src/utils/triangle.cc
@@ -5,7 +5,7 @@ Triangle::Triangle(Point3& a, Point3& b, Point3& c, Texture_Material* texture)
   b(b),
   c(c),
   texture(texture),
-  normal(Vector3(a, b) * Vector3(a, c))
+  normal(crossProd(Vector3(a, b), Vector3(a, c)))
 {
     normal.normalize();
 }
@@ -13,7 +13,43 @@ Triangle::Triangle(Point3& a, Point3& b, Point3& c, Texture_Material* texture)
 std::optional<Vector3> Triangle::intersect(const Vector3& origin,
     const Vector3& direction) const
 {
-    float D = dotProd(normal, a);
+    // algorithme de Moller-Trumbore
+    const float EPSILON = 0.0000001;
+    Vector3 vertex0 = a;
+    Vector3 vertex1 = b;
+    Vector3 vertex2 = c;
+    Vector3 edge1, edge2, h, s, q;
+    float A, f, u, v;
+    edge1 = vertex1 - vertex0;
+    edge2 = vertex2 - vertex0;
+    h = crossProd(direction, edge2);
+    A = dotProd(edge1, h);
+    if (A > -EPSILON && A < EPSILON) {
+        return std::nullopt;
+    }
+
+    f = 1.0 / A;
+    s = origin - vertex0;
+    u = f * dotProd(s, h);
+    if (u < 0.0 || u > 1.0) {
+        return std::nullopt;
+    }
+
+    q = crossProd(s, edge1);
+    v = f * dotProd(direction, q);
+    if (v < 0.0 || u + v > 1.0) {
+        return std::nullopt;
+    }
+
+    float t = f * dotProd(edge2, q);
+    if (t > EPSILON) {
+        Vector3 intersection = origin + (direction * t);
+        return intersection;
+    } else {
+        return std::nullopt;
+    }
+
+    /*float D = dotProd(normal, a);
     //Here we do not need to negate since the z vector of the camera
     //and projection vector have the same orientation
     float t = - (dotProd(normal, origin) + D) / dotProd(normal, direction);
@@ -33,6 +69,7 @@ std::optional<Vector3> Triangle::intersect(const Vector3& origin,
     }
 
     return std::nullopt;
+     */
 }
 
 Vector3 Triangle::get_normal(const Point3& p) const
